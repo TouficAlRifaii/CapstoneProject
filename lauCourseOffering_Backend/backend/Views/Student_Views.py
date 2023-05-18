8e2e13a7ce73847b07921aa45dd2e7748287a6b8
TouficAlRifaii <toufic.alrifaii@lau.edu>
2023-05-19 02:16:03 +0300

    Cleaning the code more

--- lauCourseOffering_Backend/backend/Views/Student_Views.py
+++ lauCourseOffering_Backend/backend/Views/Student_Views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..serializers import CourseSerializer, StudentSerializer
 from ..models import Course, Student, Major
-from ..excelOps import readExcel
+from openpyxl import load_workbook
 
 
 class StudentsApi(APIView):
@@ -51,3 +51,29 @@ class StudentsApi(APIView):
         return Response({
             "message": "success"
         })
+
+
+def readExcel(file):
+    book = load_workbook(file)
+    sheet = book.active
+
+    rows = sheet.rows
+
+    headers = [cell.value for cell in next(rows)]
+    all_rows = []
+    for row in rows:
+        data = {}
+        courses = []
+
+        for title, cell in zip(headers, row):
+            index = row.index(cell)
+            if index > 0 and index <= 3:
+                data[title] = cell.value
+
+            elif index > 3:
+                if cell.value == 1 or cell.value == 0:
+                    courses.append(title)
+
+        data['courses'] = courses
+        all_rows.append(data)
+    return all_rows
