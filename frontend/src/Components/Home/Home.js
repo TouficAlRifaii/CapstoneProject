1b5dec9831a016c2c4d94cb2174ed8e059b38910
TouficAlRifaii <toufic.alrifaii@lau.edu>
2023-05-19 08:35:37 +0300

    Linking course offering

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -4,64 +4,7 @@ import CourseOffering from "./CourseOffering";
 import TimeTableComponent from "./TimeTable";
 import "../../CSS/Home.css";
 const Home = ({ courses }) => {
-  const [sections, setSections] = useState([
-    {
-      id: 1,
-      campus: "Byblos",
-      numOfStudents: 21,
-      numOfSections: 1,
-      course: 1,
-      capacity: 40,
-    },
-    {
-      id: 1143,
-      campus: "Beirut",
-      numOfStudents: 31,
-      numOfSections: 1,
-      course: 43,
-      capacity: 40,
-    },
-    {
-      id: 1144,
-      campus: "Byblos",
-      numOfStudents: 31,
-      numOfSections: 1,
-      course: 44,
-      capacity: 40,
-    },
-    {
-      id: 1145,
-      campus: "Beirut",
-      numOfStudents: 51,
-      numOfSections: 1,
-      course: 44,
-      capacity: 40,
-    },
-    {
-      id: 1146,
-      campus: "Beirut",
-      numOfStudents: 181,
-      numOfSections: 4,
-      course: 45,
-      capacity: 40,
-    },
-    {
-      id: 1147,
-      campus: "Byblos",
-      numOfStudents: 48,
-      numOfSections: 1,
-      course: 45,
-      capacity: 40,
-    },
-    {
-      id: 1148,
-      campus: "Byblos",
-      numOfStudents: 19,
-      numOfSections: 1,
-      course: 46,
-      capacity: 40,
-    },
-  ]);
+  const [sections, setSections] = useState([]);
   //if true shows upload Exel, else shows CourseOffering
   const [activeOffering, setActiveOffering] = useState(true);
 
@@ -74,7 +17,12 @@ const Home = ({ courses }) => {
         setSections={setSections}
         courses={courses}
       />
-      <UploadCSV active={activeOffering} setActive={setActiveOffering} />
+      <UploadCSV
+        active={activeOffering}
+        setActive={setActiveOffering}
+        sections={sections}
+        setSections={setSections}
+      />
     </div>
   );
 };
